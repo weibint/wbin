17a9d8703d39e456a65933e000d191cf469211aa
root <weibint1981@163.com>
2016-07-21 16:34:06 +0800

    add the calFun01() for swap the value

--- cal.cpp
+++ cal.cpp
@@ -4,9 +4,22 @@
 
 using namespace std;
 
+int calFun01(int* i, int* j)
+{
+	if(*i > *j)
+	{
+		int temp = *i;
+		*i = *j;
+		*j = temp;
+	}
+	return 0;
+}
+
 int main()
 {
-	int i,j;
+	int i,j,k,m,n;
+	m = 999;
+	n = 99;
 	i=((0&1)^1);
     const char* ch1="unkonw";
 	const char* ch2="abc";
@@ -25,6 +38,9 @@ int main()
 	cout<<"5&3:"<<(5&3)<<endl;
 	cout<<"5^3:"<<(5^3)<<endl;
 
+	cout<<"before cal: m="<<m<<","<<"n="<<n<<endl;
+	k = calFun01(&m,&n);
+	cout<<"after cal: m="<<m<<","<<"n="<<n<<endl;
 
 	cout<<"***************"<<endl;
 	cout<<"5%2:"<<5%2<<endl;
